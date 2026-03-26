2144a13461f60993ebe663a6d36e5cd37fad69a6
ShruthiGit <shruthinarasimhan3.com>
2026-03-26 10:20:28 +0530

    Updated remaining files

--- Day_7/Concepts_Code/SimpleInheritance.java
+++ Day_7/Concepts_Code/SimpleInheritance.java
@@ -1,15 +1,14 @@
 package Day_7.Concepts_Code;
- class Parent{
-    void display(){
-        System.out.println("This is a parent class");
+class Parent{
+        void display(){
+            System.out.println("This is a parent class");
+        }
     }
-}
- class Child extends Parent{
-    void display1(){
-        System.out.println("This is a child class");
+    class Child extends Parent{
+        void display1(){
+            System.out.println("This is a child class");
+        }
     }
-}
-
 public class SimpleInheritance {
     public static void main(String[] args) {
         Child child = new Child();
