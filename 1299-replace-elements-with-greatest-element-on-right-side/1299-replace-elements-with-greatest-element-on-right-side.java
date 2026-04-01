bbd11bf48c9a0cac09c207221ac9669760fb20fa
ShruthiGit <shruthinarasimhan3@gmail.com>
2026-04-01 18:19:26 +0530

    Time: 2 ms (99.78%), Space: 49.2 MB (40.5%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
@@ -1,24 +1,13 @@
 class Solution {
     public int[] replaceElements(int[] arr) {
-        List<Integer> list=new ArrayList<>();
         int n=arr.length;
-        if(n==1)
-        {
-            return new int[]{-1};
-        }
         int max=-1;
         for(int i=n-1;i>=0;i--)
         {
-            list.add(max);
-            if(arr[i]>max)
-            {
-                max=arr[i];
-            }
-        }
-        Collections.reverse(list);
-        for(int i=0;i<list.size();i++)
-        {
-            arr[i]=list.get(i);
+            int current=arr[i];
+            arr[i]=max;
+            if(current>max)
+            max=current;
         }
         return arr;
     }
