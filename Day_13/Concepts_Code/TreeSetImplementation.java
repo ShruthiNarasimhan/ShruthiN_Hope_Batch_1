e3a5344ad4f3d1c88d5b810e053159ba0536cb2c
ShruthiGit <shruthinarasimhan3.com>
2026-04-07 21:43:09 +0530

    Added New Files

--- Day_13/Concepts_Code/TreeSetImplementation.java
+++ Day_13/Concepts_Code/TreeSetImplementation.java
@@ -2,11 +2,12 @@ package Day_13.Concepts_Code;
 import java.util.*;
 public class TreeSetImplementation {
     public static void main(String[] args) {
-        Set<Integer> set = new TreeSet<>();
+        TreeSet<Integer> set = new TreeSet<>();
         set.add(30);
         set.add(20);
         set.add(10);
         set.add(15);
+    set.tailSet(5);
         System.out.println(set);
     }
     
