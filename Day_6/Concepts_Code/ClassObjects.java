2bc65e60f69b34ab41777f19b5fa9225856baf1b
ShruthiGit <shruthinarasimhan3.com>
2026-04-10 21:40:02 +0530

    Added New Files

--- Day_6/Concepts_Code/ClassObjects.java
+++ Day_6/Concepts_Code/ClassObjects.java
@@ -3,11 +3,11 @@ package Day_6.Concepts_Code;
 public class ClassObjects {
     public static void main(String[] args) {
         System.out.println("This is a class and object concept in java");  
-        Apartment ap = new Apartment();
+        SimpleApartment ap = new SimpleApartment();
         ap.display();
     }     
 }
-class Apartment{
+class SimpleApartment{
     public void display(){
         System.out.println("Im Inside the apartment class");
     }
