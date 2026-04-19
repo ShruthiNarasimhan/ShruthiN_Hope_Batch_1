e5ca91f22d3b91b494ff840fdef40c0e363c56d6
ShruthiGit <shruthinarasimhan3@gmail.com>
2026-04-19 22:08:25 +0530

    Time: 3 ms (99.92%), Space: 46.3 MB (80.96%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,10 +1,10 @@
 class Solution {
-    public String intToRoman(int num) {
-        String M[] = { "", "M", "MM", "MMM" };
-        String C[] = { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
-        String X[] = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
-        String I[] = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
-        return M[num / 1000] + C[(num % 1000) / 100] + X[(num % 100) / 10] + I[num % 10];
+  static String[] c = new String[]{"","I","II","III","IV","V","VI","VII","VIII","IX"};
+    static String[] tens = new String[]{"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+    static String[] hundreds = new String[]{"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+    static String[] thousands = new String[]{"","M","MM","MMM"};
+    public static String intToRoman(int num) {
+        return new StringBuilder().append(thousands[num/1000]).append(hundreds[(num%1000)/100]).append(tens[(num%100)/10]).append(c[num%10]).toString();
 
     }
 }
