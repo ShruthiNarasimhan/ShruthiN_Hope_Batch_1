a1ec993cef77764fd0a02a12c7650dcc344cd25d
ShruthiGit <shruthinarasimhan3@gmail.com>
2026-04-22 18:57:09 +0530

    Time: 1 ms (97.81%), Space: 42.1 MB (98.57%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,20 +1,6 @@
-class Solution {
-    public int mySqrt(int x) {
-        if(x<2) return x;
-        int low=1,high=x/2,ans=0;
-        while(low<=high)
-        {
-            int mid=(low+high)/2;
-            if(mid<=x/mid)
-            {
-                ans=mid;
-                low=mid+1;
-            }
-            else
-            {
-                high=mid-1;
-            }
-        } 
-        return ans;
+public class Solution{
+    public int mySqrt(int x)
+    {
+        return (int)Math.sqrt(x);
     }
 }
