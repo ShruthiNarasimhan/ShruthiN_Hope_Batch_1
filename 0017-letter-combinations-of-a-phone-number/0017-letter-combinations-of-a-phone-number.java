160fa2280ea0b29db47374d0f79f46fdc8af9981
ShruthiGit <shruthinarasimhan3@gmail.com>
2026-04-27 17:01:47 +0530

    Time: 0 ms (100%), Space: 43.7 MB (41.9%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,21 +1,35 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-        if (digits.isEmpty()) return Collections.emptyList();
+      
+        List<String> result = new ArrayList<>();
 
-        String[] phone_map = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        List<String> output = new ArrayList<>();
-        alpha("", digits, phone_map, output);
-        return output;
+        if( digits == null || digits.length() == 0){
+            return result;
+        }
+
+        String[] map = {
+            "", "", "abc", "def", "ghi", "jkl",
+            "mno", "pqrs", "tuv", "wxyz"
+        };
+
+        alpha(digits, 0, new StringBuilder(), result, map);
+       
+        return result;  
     }
 
-    private void alpha(String combination, String next_digits, String[] phone_map, List<String> output) {
-        if (next_digits.isEmpty()) {
-            output.add(combination);
-        } else {
-            String letters = phone_map[next_digits.charAt(0) - '2'];
-            for (char letter : letters.toCharArray()) {
-                alpha(combination + letter, next_digits.substring(1), phone_map, output);
-            }
+    void alpha(String digits, int index, StringBuilder path, List<String> result, String[] map){
+
+        if(index == digits.length()){
+            result.add(path.toString());
+            return;
+        }
+
+        String letters = map[digits.charAt(index) - '0'];
+
+        for(char ch : letters.toCharArray()){
+            path.append(ch);
+            alpha(digits, index + 1, path, result, map);
+            path.deleteCharAt(path.length() - 1);
         }
     }
 }
